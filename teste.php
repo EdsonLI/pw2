f633506890985daeaba7860ededb7d92a611cd26
Edson Luis Isele <edsonisele@outlook.com>
2016-11-15 00:13:36 -0200

    Modificado teste.php

--- teste.php
+++ teste.php
@@ -3,48 +3,38 @@
 DESENVOLVIDO POR EDGAR SERRA
 http://www.edgarserra.com
 */
-function calcula_frete($servico, $cep_origem, $cep_destino, $peso, $mao_propria, $valor_declarado, $aviso_recebimento){
+function calcula_frete($servico, $cep_origem, $cep_destino, $peso, $mao_propria, $valor_declarado, $aviso_recebimento) {
+    $mao_propria = (strtolower($mao_propria) == 's') ? 's' : 'n';
+    $aviso_recebimento = (strtolower($aviso_recebimento) == 's') ? 's' : 'n';
 
-$mao_propria = (strtolower($mao_propria) == 's') ? 's' : 'n';
-$aviso_recebimento = (strtolower($aviso_recebimento) == 's') ? 's' : 'n';
+    $url = 'http://ws.correios.com.br/calculador/CalcPrecoPrazo.aspx?nCdEmpresa=&sDsSenha=&sCepOrigem='.$cep_origem.'&sCepDestino='.$cep_destino.'&nVlPeso='.$peso.'&nCdFormato=1&nVlComprimento=20&nVlAltura=5&nVlLargura=15&sCdMaoPropria='.$mao_propria.'&nVlValorDeclarado='.$valor_declarado.'&sCdAvisoRecebimento='.$aviso_recebimento.'&nCdServico='.$servico.'&nVlDiametro=0&StrRetorno=xml';
 
-$url = 'http://ws.correios.com.br/calculador/CalcPrecoPrazo.aspx?nCdEmpresa=&sDsSenha=&sCepOrigem='. $cep_origem .'&sCepDestino='. $cep_destino .'&nVlPeso='. $peso .'&nCdFormato=1&nVlComprimento=20&nVlAltura=5&nVlLargura=15&sCdMaoPropria='. $mao_propria .'&nVlValorDeclarado='. $valor_declarado .'&sCdAvisoRecebimento='. $aviso_recebimento .'&nCdServico='. $servico .'&nVlDiametro=0&StrRetorno=xml';
+    $frete_calcula = simplexml_load_string(file_get_contents($url));
+    /*
+    CASO QUEIRA VER TUDO QUE VEM DO SITE DOS CORREIOS, DESCOMENTE A LINHA ABAIXO.
+    */
+    echo print_r($frete_calcula);
+    echo '<br/><br/>';
 
-$frete_calcula = simplexml_load_string(file_get_contents($url));
-/*
-CASO QUEIRA VER TUDO QUE VEM DO SITE DOS CORREIOS, DESCOMENTE A LINHA ABAIXO.
-*/
-echo print_r($frete_calcula);
-echo '<br/><br/>';
+    $frete = $frete_calcula->cServico;
 
-$frete = $frete_calcula->cServico;
-
-if($frete->Erro == '0'){
-
-    switch($frete->Codigo){
-        case 41106: $servico = 'PAC'; break;
-        case 40045: $servico = 'SEDEX a Cobrar'; break;
-        case 40215: $servico = 'SEDEX 10'; break;
-        case 40290: $servico = 'SEDEX Hoje'; break;
-        default: $servico = 'SEDEX';
+    if($frete->Erro == '0') {
+        switch($frete->Codigo) {
+            case 41106: $servico = 'PAC'; break;
+            case 40045: $servico = 'SEDEX a Cobrar'; break;
+            case 40215: $servico = 'SEDEX 10'; break;
+            case 40290: $servico = 'SEDEX Hoje'; break;
+            default: $servico = 'SEDEX';
+        }
+        $retorno = 'Serviços: '.$servico.'<br>';
+        $retorno .= 'Valor: '.$frete->Valor.'<br>';
+        $retorno .= 'Prazo de entrega: '.$frete->PrazoEntrega.' dia(s)';
+    } elseif($frete->Erro == '7') {
+        $retorno = 'Serviço temporariamente indisponível, tente novamente mais tarde.';
+    } else {
+        $retorno = 'Erro no cálculo do frete, código de erro: '.$frete->Erro;
     }
-
-    $retorno = 'Serviços: '.$servico.'<br>';
-    $retorno .= 'Valor: '.$frete->Valor.'<br>';
-    $retorno .= 'Prazo de entrega: '.$frete->PrazoEntrega.' dia(s)';
-
-} elseif($frete->Erro == '7') {
-
-    $retorno = 'Serviço temporariamente indisponível, tente novamente mais tarde.';
-
-} else {
-
-    $retorno = 'Erro no cálculo do frete, código de erro: '.$frete->Erro;
-
-}
-
-return $retorno;
-
+    return $retorno;
 }
 /*
 A função é composta dos seguintes itens:
@@ -72,5 +62,5 @@ n - Mão própria
 700 - Valor declarado (R$7,00)
 s - Aviso de recebimento
 */
-	echo calcula_frete('40290','97032120','99150000','2','n','700','s');
+	echo calcula_frete('41106', '97032120', '99150000', '2', 's', '700', 's');
 ?>
