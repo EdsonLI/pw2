65e45d952ab7992921c0117af6249e40784f42d5
unknown <edsonisele@outlook.com>
2016-11-17 17:08:02 -0200

    Melhoria para mensagem de erro por informacao de CEP invalido

--- pw2_calcFrete/calcFrete_js.js
+++ pw2_calcFrete/calcFrete_js.js
@@ -3,27 +3,22 @@ $().ready(function() {
     //--------------------LINK EXCLUIR--------------------------
     $(document).on('click', '.calcFrete', function(event) {
         event.preventDefault();
-        console.info('chamou o calcFrete');
-
-//        $confirmaExcCarro = true;
-//
-//        if($confirmaExcCarro == true) {
-            $.ajax({
-               url: 'calcFrete_control.php',
-               dataType: 'html',
-               type: 'POST',
-               data: { acao: 'calcFrete', cep: $("#CEP").val()  },
-               success: function(dados) {
-                   console.info('obteve retorno do calcFrete');
-                   console.info('retorno: ', dados);
-                   console.info($("#CEP").val());
-//                   self.closest("tr").remove();
-                   $("#tipo_entrega").html(dados);
-//                   openModalMsg();
-//                   setTimeout(closeModalMsg, 3000);
-                   //limpar();
-               }
-            });
-//        }
+        $cep = $("#CEP").val();
+        if($cep == "") {
+            $("#tipo_entrega").html('<br><label class="alert alert-danger">Por favor informe um CEP válido para calcular o frete!</label>');
+            return;
+        }
+        $.ajax({
+           url: 'calcFrete_control.php',
+           dataType: 'html',
+           type: 'POST',
+           data: { acao: 'calcFrete', cep: $cep  },
+           success: function(dados) {
+               //console.info('obteve retorno do calcFrete');
+               //console.info('retorno: ', dados);
+               //console.info($("#CEP").val());
+               $("#tipo_entrega").html(dados);
+           }
+        });
     });
 });
